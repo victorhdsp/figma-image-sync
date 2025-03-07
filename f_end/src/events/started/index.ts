5c32cac8a1c6a5d0dd853f3efb9365aef89d3063
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2025-03-07 15:56:35 -0300

    feature, sincronize images
    Agora as imagens só são baixadas se elas foram modificadas na pasta do drive, do contrário ela só é ignorada, porém ainda existe o problema de que o plugin não roda em background o que faz parte de um bom funcionamento.

---
+++ f_end/src/events/started/index.ts
@@ -0,0 +1,74 @@
+import google, { IFile } from "../../utils/google";
+import { createComponent, getComponent, componentNeedUpdate, setImageInComponent } from "./component";
+
+async function create_or_set_page(pluginId: string) {
+  await figma.loadAllPagesAsync();
+  const pages = figma.root.children;
+  let page = pages.find((page) => page.name === pluginId);
+  if (!page) {
+    page = figma.createPage();
+    page.name = pluginId;
+  }
+  return page;
+}
+
+async function addImageInPage(file: IFile, page: PageNode, token: string) {
+  const imageInfo = await google.get_image_info(token, file.id);
+  if (!imageInfo) {
+    figma.notify("Error to get image info");
+    return false;
+  }
+
+  let fComp: ComponentNode | null;
+  fComp = await getComponent(page, imageInfo);
+
+  if (fComp) {
+    if (!await componentNeedUpdate(fComp, imageInfo))
+      return true;
+  } else
+    fComp = await createComponent(imageInfo);
+  
+  if (!fComp) {
+    figma.notify("Strange error in component");
+    return false;
+  }
+
+  const base64 = await google.download_image(token, file.id);
+  if (!base64) {
+    figma.notify("Error to download image");
+    return false;
+  }
+  await setImageInComponent(fComp, base64);
+  return true;
+}
+
+async function exec(folderId: string, page: PageNode, token: string) {
+  const files = await google.list_images_and_folders(token, folderId);
+  
+  if (!files) {
+    figma.notify("No files found");
+    return;
+  }
+
+  while (files.length > 0) {
+    const file = files.pop();
+    if (file)
+      if (file?.mimeType === "application/vnd.google-apps.folder") {
+        const folderFiles = await google.list_images_and_folders(token, file.id);
+        if (folderFiles)
+          files.push(...folderFiles);
+      } else {
+        await addImageInPage(file, page, token);
+      }
+  }
+}
+
+export async function started() {
+  const pluginId = figma.root.getPluginData("plugin_id");
+  const folderId = await figma.clientStorage.getAsync(`${pluginId}_folder`);
+  const token = await figma.clientStorage.getAsync('token');
+  const page = await create_or_set_page(folderId);
+  exec(folderId, page, token);
+}
+
+export const STARTED = "started";
